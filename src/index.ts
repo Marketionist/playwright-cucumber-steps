8581b2496e60dfe617d18b28de8f8567c1f68df7
Marketionist <marketionist@gmail.com>
2026-08-06 22:23:29 -0400

    Add 'CSS property should be' steps

--- src/index.ts
+++ src/index.ts
@@ -1027,6 +1027,48 @@ Then('{word} from {word}( page) attribute {string} should not contain {word} fro
         .not.toHaveAttribute(attribute, regularExpression);
 });
 
+Then('{string}.{string} CSS property {string} should be {string}', async (
+    { page, }, pageObject: string, element: string, cssProperty: string, value: string
+) => {
+    const regularExpression = new RegExp(`^${value}$`, 'g');
+
+    await expect(page.locator(pageObjects[pageObject][element])).toHaveCSS(cssProperty, regularExpression);
+});
+
+Then('{word} from {word}( page) CSS property {string} should be {string}', async (
+    { page, }, element: string, pageObject: string, cssProperty: string, value: string
+) => {
+    const regularExpression = new RegExp(`^${value}$`, 'g');
+
+    await expect(page.locator(pageObjects[pageObject][element])).toHaveCSS(cssProperty, regularExpression);
+});
+
+Then('{string}.{string} CSS property {string} should be {string}.{string}', async (
+    { page, },
+    pageObject1: string,
+    element1: string,
+    cssProperty: string,
+    pageObject2: string,
+    element2: string
+) => {
+    const regularExpression = new RegExp(`^${pageObjects[pageObject2][element2]}$`, 'g');
+
+    await expect(page.locator(pageObjects[pageObject1][element1])).toHaveCSS(cssProperty, regularExpression);
+});
+
+Then('{word} from {word}( page) CSS property {string} should be {word} from {word}( page)', async (
+    { page, },
+    element1: string,
+    pageObject1: string,
+    cssProperty: string,
+    element2: string,
+    pageObject2: string
+) => {
+    const regularExpression = new RegExp(`^${pageObjects[pageObject2][element2]}$`, 'g');
+
+    await expect(page.locator(pageObjects[pageObject1][element1])).toHaveCSS(cssProperty, regularExpression);
+});
+
 Then('{string}.{string} CSS property {string} should contain {string}', async (
     { page, }, pageObject: string, element: string, cssProperty: string, value: string
 ) => {
